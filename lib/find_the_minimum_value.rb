39e15a737d8aab080d299a35e26a538fdf2abd53
Anmolpreet Kaur <anmolpreet.kaur@yale.edu>
2020-05-12 04:02:59 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,5 +1,13 @@
 def find_min_value(array)
   count = 0 
   potential_min = 10000
-  while count < 
+  while count < array.length do 
+    if potential_min > array[count]
+      potential_min = array[count]
+    end 
+    count += 1
+  end 
+  potential_min
 end
+
+
